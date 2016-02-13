cff733ce72393e395a16b007215211081aee057a
Harshikesh Kumar <harshikesh.kumar@olacabs.com>
2016-02-13 20:09:00 +0530

    BottomFragment changes

--- app/src/main/java/com/save/mycity/ui/fragment/DetailFragment.java
+++ app/src/main/java/com/save/mycity/ui/fragment/DetailFragment.java
@@ -27,7 +27,7 @@ public class DetailFragment extends Fragment {
   @Nullable @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
 
-    View view  = inflater.inflate(R.layout.activity_splash, container, false);
+    View view  = inflater.inflate(R.layout.fragment_detail, container, false);
 
     return view;
   }
