0f2472f8dd4c822b8a15c48edbd852d4c823e87e
Harshikesh Kumar <harshikesh.kumar@olacabs.com>
2016-02-13 23:10:10 +0530

    CreateIssueFragment implemented

--- app/src/main/java/com/save/mycity/ui/activity/CreateIssueActivity.java
+++ app/src/main/java/com/save/mycity/ui/activity/CreateIssueActivity.java
@@ -2,16 +2,33 @@ package com.save.mycity.ui.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Toast;
 import com.save.mycity.R;
 
 /**
  * Created by harshikesh.kumar on 13/02/16.
  */
-public class CreateIssueActivity extends Activity {
+public class CreateIssueActivity extends Activity implements View.OnClickListener{
+
+  private FloatingActionButton mSaveButton;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_category);
+    mSaveButton = (FloatingActionButton) findViewById(R.id.save);
 
+  }
+
+  @Override public void onClick(View v) {
+
+    switch (v.getId())
+    {
+      case R.id.save:
+        Toast.makeText(getApplicationContext()," You will be heard! ", Toast.LENGTH_SHORT);
+        finish();
+        break;
+    }
   }
 }
